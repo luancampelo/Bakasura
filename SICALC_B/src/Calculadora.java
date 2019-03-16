f447096fa8200508d1b215162d0e56f7b48f33f6
1binfo <1binfo@DESKTOP-JN09DPS.basilides.local>
2019-03-15 22:31:29 -0300

    Criação do método somar turma B

---
+++ SICALC_B/src/Calculadora.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+
+	private int valor1;
+	private int valor2;
+
+	public Calculadora() {
+
+	}
+	public Calculadora(int valor1, int valor2) {
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+	}
+	public int getValor1() {
+		return valor1;
+	}
+	public void setValor1(int valor1) {
+		this.valor1 = valor1;
+	}
+	public int getValor2() {
+		return valor2;
+	}
+	public void setValor2(int valor2) {
+		this.valor2 = valor2;
+	}
+
+	public void somar () {
+		valor1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o Valor1"));
+		valor2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o Valor2"));
+
+		System.out.println("A soma dos valores eh = " + (valor1 + valor2));
+	}
+
+
+}
